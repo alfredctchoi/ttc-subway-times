75c82d9efd561fdf414fbc6793a67534b3b9a0bd
Alfred Choi <Alfred.Choi@scotiabank.com>
2017-04-24 10:16:10 -0400

    Initial Commit

---
+++ ttc-subway-times/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ttc-subway-times
+//
+//  Created by Alfred Choi on 2017-04-24.
+//  Copyright © 2017 Alfred Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
